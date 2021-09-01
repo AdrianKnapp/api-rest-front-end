27c68dd9115ea065b83faaaaf9fb2db3276528ce
Adrian <adrknapp@gmail.com>
2021-08-31 22:34:57 -0300

    Changing async function, added try and catch

--- src/js/script.js
+++ src/js/script.js
@@ -2,13 +2,18 @@ const userEmail = document.querySelector('.email');
 const userName = document.querySelector('.name');
 
 async function fetchPage() {
-  const userId = 1;
-  const response = await fetch(`./api/user/${userId}`);
-  const responseData = await response.json();
+  try {
+    const userId = 1;
+    const response = await fetch(`./api/user/${userId}`);
+    const responseData = await response.json();
 
-  const { data } = responseData;
+    const { data } = responseData;
 
-  userEmail.innerHTML = data.email;
-  userName.innerHTML = data.name;
+    userEmail.innerHTML = data.email;
+    userName.innerHTML = data.name;
+    
+  } catch (error) {
+    console.log(error);
+  }
 }
 fetchPage();
